d1e37fd2157188069a4476eef222a240a805c380
Jeet Patel <jeetpatel499@Jeets-MacBook-Air.local>
2023-05-27 00:37:53 -0400

    first commit

---
+++ Task2.R
@@ -0,0 +1,22 @@
+library(ggplot2)
+library(cluster)
+df <- iris
+head(iris)
+
+ggplot(df, aes(Petal.Length,Petal.Width)) + geom_point(aes(col=Species),size=2)
+
+set.seed(101)
+irisCluster <- kmeans (df[,1:4], centers = 3, nstart = 20)
+irisCluster
+
+table(irisCluster$cluster, df$Species)
+
+clusplot(iris, irisCluster$cluster, color = T,shade = T,labels = 0,lines = 0)
+
+tot.withinss <- sapply(1:10, function(i) {
+  irisCluster <- kmeans(df[,1:4], center=i, nstart=20)
+  return(irisCluster$tot.withinss)
+})
+
+
+plot(1:10, tot.withinss, type = "b",pch = 19, xlab = "Number of Clusters", ylab = "Total Within-Cluster Sum of Squares")
